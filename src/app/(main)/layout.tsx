445fa6421c0779928b3915a08f05839930ac8e46
Fuyad Hasan Fahim <codewithfuyad@gmail.com>
2025-06-23 23:51:10 +0600

    Done the navbar, but need to do more things.

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,12 @@
+import { Navbar } from '@/components/shared/navbar';
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return <main>{children}</main>;
+    return (
+        <>
+            <header>
+                <Navbar />
+            </header>
+            {children}
+        </>
+    );
 }
